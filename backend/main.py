4755d245e058a5ea41ae349c7ffc1e91aec23548
Zayaad Wajid <zayaadw@gmail.com>
2025-11-17 23:56:38 +0500

    Initialized Docker setup

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,8 @@
+# filepath: c:\Users\Zayad\Documents\FYP\01_WORK\ACDS-FYP\backend\main.py
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
